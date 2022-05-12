d092cb912be19453d0b42798910c2e2b17bddbc1
Murugan2207 <vickyvenkat2207@gmail.com>
2022-05-12 09:47:52 +0530

    added update features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { MovielistComponent } from './movielist/movielist.component';
 import { RegisterComponent } from './register/register.component';
 import { RoleGuard } from './role.guard';
+import { UpdateUsersComponent } from './update-users/update-users.component';
 import { ViewUsersComponent } from './view-users/view-users.component';
 
 const routes: Routes = [
@@ -17,6 +18,7 @@ const routes: Routes = [
     {path:'movielist',component:MovielistComponent},
     {path:'view-users',component:ViewUsersComponent,canActivate:[AuthGuard,RoleGuard]},
     {path:'add-movies',component:AddMoviesComponent},
+    {path:'update-users/:id',component:UpdateUsersComponent},
     {path:'',redirectTo:'home',pathMatch:'full'}
 ];
 
--- src/app/update-users/update-users.component.ts
+++ src/app/update-users/update-users.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-update-users',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-users.component.css']
 })
 export class UpdateUsersComponent implements OnInit {
+  user:any;
+  id:any;
 
-  constructor() { }
+  constructor(private http:HttpClient,private toastr:ToastrService,private route:ActivatedRoute) {
+    this.id=this.route.snapshot.params["id"];
+   }
 
   ngOnInit(): void {
+    this.getUser();    
   }
+  getUser(){
+    const url="http://localhost:8080/users/find/"+this.id;
+    this.http.get(url).subscribe(res=>{
+      this.user=res;
+    })
+  }
+  updateUser(){
+    console.log(this.user);
+    const url="http://localhost:8080/users/update/"+this.id;
+    this.http.put(url,this.user).subscribe(res=>{
+      this.toastr.success('updated');
+    })
+  }
+  
 
 }
